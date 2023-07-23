b8a8abd78b55ac6a0714b41131c8146db4b814e6
George8918 <GZheng8918@gmail.com>
2023-07-22 18:16:07 -0700

    complete

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,10 @@
 
 class Person:
     # Class body goes here
-
+  def talk(id):
+    print("Hello World!")
     #Instance method definition
     pass
+
+  def walk(id):
+    print("The person is walking.")
